4103b8eb31404618d9ae2521d04e89f9ca86c5d4
Steve Goldstein <sgoldstein@wisc.edu>
2020-11-19 16:53:07 -0600

    WIP

---
+++ lib/pullData.R
@@ -0,0 +1,62 @@
+#############
+
+
+####
+fetchWIData <- function(rawData) {
+    url <- "https://opendata.arcgis.com/datasets/5374188992374b318d3e2305216ee413_12.csv"
+    df <- read.csv(url, fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE) 
+
+    ## archive data pull
+    if (!is.null(rawData)) {
+        saveRDS(df, rawData)
+    }
+
+    return(df)
+}  ## fetchWIData
+
+formatWIData <- function(df) {
+    df <-
+        df %>%
+        clean_names() %>% 
+        select(name:test_new) %>% 
+        mutate(date = ymd_hms(date)) %>% 
+        group_by(name) %>% 
+        arrange(desc(date)) %>%
+
+        # enforce monotonicity  
+        mutate(positive = cummin(positive),
+               negative = cummin(negative),
+               deaths = cummin(deaths)) %>% 
+        arrange(date) %>% 
+
+    # smooth positive rate
+    mutate(pos_new = positive - lag(positive, 1, 0),
+           neg_new = negative - lag(negative, 1, 0),
+           dth_new = deaths - lag(deaths, 1, 0)) %>% 
+    mutate(tests = positive + negative,
+           test_new = pos_new + neg_new,
+           test_new = na.fill(test_new, "extend"),
+           pos_rate = pos_new / test_new,
+           pos_rate = na.fill(pos_rate, "extend")) %>% 
+    nest() %>% 
+    mutate(pos_rate_gam = map(data, function(df) fitted(gam(pos_rate ~ s(as.numeric(date)), data = df, family = "quasibinomial", weights = test_new)))) %>%
+    unnest(cols = c(data, pos_rate_gam)) %>% 
+    group_by(date) %>% 
+
+                                        # scale cases by positive rate
+    mutate(case = pos_new * (pos_rate_gam/quantile(pos_rate_gam, probs = 0.025, na.rm = TRUE))^0.1) %>% 
+    ungroup()
+
+    return(df)
+}  ## formatWIData
+
+pullData <- function(state,rawData = NULL) {
+    if (state == "WI") {
+        df_raw <- fetchWIData(rawData)
+        df <- formatWIData(df_raw)
+        return(df)
+    }
+}
+        
+
+    
